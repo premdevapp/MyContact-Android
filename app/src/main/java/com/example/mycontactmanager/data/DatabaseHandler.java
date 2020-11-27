888b352d033992d9f0681c0770bcba6bed12f040
premnath <premdevapp@gmail.com>
2020-11-27 09:33:52 +0530

    modified 1

--- app/src/main/java/com/example/mycontactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/example/mycontactmanager/data/DatabaseHandler.java
@@ -6,19 +6,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.mycontactmanager.R;
+import com.example.mycontactmanager.util.Util;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler( Context context,  String name,  SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_CONTACT_TABLE = String.format("CREATE TABLE %s(%d INTEGER PRIMARY KEY, %s TEXT, %s TEXT)", Util.TABEL_NAME, Util.KEY_ID, Util.KEY_NAME, Util.KEY_PHONE_NUMBER);
+        db.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        String DROP_TABLE = String.valueOf((R.string.db_drop));
+        db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
+        onCreate(db);
     }
 }
---
+++ app/src/main/java/com/example/mycontactmanager/util/Util.java
@@ -0,0 +1,15 @@
+package com.example.mycontactmanager.util;
+
+public class Util {
+
+    //database
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "contact_db";
+    public static final String TABEL_NAME = "contacts";
+
+    // tabel
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_PHONE_NUMBER = "phone number";
+
+}
